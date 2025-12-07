2f95949e12b211efc850603b3eeb5b76fc46e300
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-12-07 08:13:51 +0000

    Update motion_alarm.py

--- motion_alarm.py
+++ motion_alarm.py
@@ -2,20 +2,18 @@ from gpiozero import MotionSensor, Buzzer
 from time import sleep
 
 pir = MotionSensor(4) # HC-SR501
-buzzer = Buzzer(17) # active 
+buzzer = Buzzer(17) # Active buzzer
 
-print("Waiting for HC-SR501 to settle")
+print("Waiting for PIR to settle...")
 pir.wait_for_no_motion()
 
 while True:
     print("Ready")
+    
     pir.wait_for_motion()
     print("Motion detected!")
-    buzzer.beep(0.5, 0.25, n=8)
-    sleep(3)
-
-
-
-
-
+    buzzer.beep(0.5, 0.25, n=3)
+    
+    pir.wait_for_no_motion() 
+    print("No motion.")
 
